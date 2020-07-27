496129b094de96e053f9bf55c15f99501512f9a2
hu-simon <jhhu@eng.ucsd.edu>
2020-07-27 15:09:02 -0700

    Added some test files.

--- cheapfake/scratch_26/lipreading.py
+++ cheapfake/scratch_26/lipreading.py
@@ -1,7 +1,6 @@
 """
 Python file that collects all of the LRS3 videos and extracts their frames.
 
-This Python script currently works as intended. If we want to add more directories, then they can just go in the ``create_new_directories()`` function.
 """
 
 import os
@@ -19,35 +18,10 @@ def main():
         if not f.startswith(".")
     ]
     root_path = "/home/shu/shu/Datasets/VGG_Lipreading/trainval"
-    paths = [os.path.join(root_path, suffix) for suffix in suffix_names]
-    subdirectories = [
-        "frames",
-        "media/captions/raw",
-        "media/captions/manual",
-        "media/raw_video",
-        "media/raw_audio",
-        "media/comb_video",
-    ]
-    prods = list(itertools.product(paths, subdirectories))
-    paths = ["/".join(item) for item in prods]
 
-    for path in paths:
-        try:
-            os.makedirs(path)
-        except OSError:
-            print("[INFO] Failed to create {}".format(path))
-        else:
-            print("[INFO] Successesfully created {}".format(path))
-
-    print("".join("-" * 80))
-    print("[INFO] Downloading YouTube videos")
-    print("".join("-" * 80))
-
-    urls = ["https://youtube.com/watch?v=" + item for item in suffix_names]
-    urls_subset = urls[:2]
+    urls = ["https://youtube.com/watch?v=" + suffix for suffix in suffix_names]
 
     ytdownloader = YouTubeDownloader(urls, extract_path=root_path)
-    ytdownloader.download_all_videos()
 
 
 if __name__ == "__main__":
