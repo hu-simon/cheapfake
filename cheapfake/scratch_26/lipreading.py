c3b43bf018defdcc9e23e7f456df55490b9de0cc
hu-simon <jhhu@eng.ucsd.edu>
2020-07-27 21:56:59 -0700

    Changed lipreading.py in the scratch_26 folder.

--- cheapfake/scratch_26/lipreading.py
+++ cheapfake/scratch_26/lipreading.py
@@ -14,15 +14,16 @@ from ytbot.contrib.__main__ import YouTubeDownloader
 def main():
     suffix_names = [
         f
-        for f in os.listdir("/home/shu/shu/Datasets/LRS3/trainval")
+        for f in os.listdir("/home/shu/shu/Datasets/VGG_Lipreading/trainval")
         if not f.startswith(".")
     ]
     root_path = "/home/shu/shu/Datasets/VGG_Lipreading/trainval"
 
     urls = ["https://youtube.com/watch?v=" + suffix for suffix in suffix_names]
 
-    ytdownloader = YouTubeDownloader(urls, extract_path=root_path)
-
+    ytdownloader = YouTubeDownloader(urls[0], extract_path=root_path)
+    print(ytdownloader.url_list)
+    ytdownloader._download_video(ytdownloader.url_list, name="0", verbose=True) 
 
 if __name__ == "__main__":
     main()
